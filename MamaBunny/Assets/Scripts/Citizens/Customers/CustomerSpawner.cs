2785d9eedc42baf15121dab41323e9ae350b0ddf
prunefriend <jwjmair@gmail.com>
2018-08-21 11:07:54 +1200

    Basics for customer spawner

---
+++ MamaBunny/Assets/Scripts/Citizens/Customers/CustomerSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSpawner : MonoBehaviour
+{
+
+    List <Vector3> m_TravelLocations;
+    public GameObject m_customer;
+
+    [Range(0.0f, 20.0f)]
+    public float m_minSpawnTime;
+
+    [Range(0.0f, 10.0f)]
+    public float m_randSpawnTimeOffset;
+
+    [Range(0.0f, 10.0f)]
+    public float m_spawnRadiusOffset;
+
+    private float m_nextSpawnTime;
+
+    private void Start()
+    {
+        m_nextSpawnTime = Time.time + m_minSpawnTime + Random.Range(0, m_randSpawnTimeOffset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > m_nextSpawnTime)
+        {
+            m_nextSpawnTime = Time.time + m_minSpawnTime + Random.Range(0, m_randSpawnTimeOffset);
+            Vector2 randOffset = (Random.insideUnitCircle * m_spawnRadiusOffset);
+            Vector3 startPosition = new Vector3(transform.position.x + randOffset.x, transform.position.y, transform.position.z + randOffset.y);
+            GameObject customer = Instantiate(m_customer, startPosition, transform.rotation);
+
+            //Vector3 endPosition = new Vector3(m_despawnLocation.transform.position.x + randOffset.x, m_despawnLocation.transform.position.y, m_despawnLocation.transform.position.z + randOffset.y);
+
+            // First travel location is infront of the shop
+            //m_TravelLocations.Add();
+
+            // Second location is at the counter
+        }
+    }
+}
--- MamaBunny/Assets/Scripts/Citizens/Passerbys/PasserbySpawner.cs
+++ MamaBunny/Assets/Scripts/Citizens/Passerbys/PasserbySpawner.cs
@@ -2,36 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CitizenSpawner : MonoBehaviour {
+public class PasserbySpawner : MonoBehaviour
+{
 
     public GameObject m_despawnLocation;
-    public GameObject m_citizen;
+    public GameObject m_passerby;
 
-    [Range(0.0f,20.0f)]
+    [Range(0.0f, 20.0f)]
     public float m_minSpawnTime;
 
-    [Range(0.0f,10.0f)]
+    [Range(0.0f, 10.0f)]
     public float m_randSpawnTimeOffset;
 
+    [Range(0.0f, 10.0f)]
+    public float m_spawnRadiusOffset;
+
     private float m_nextSpawnTime;
-    float m_spawnRadiusOffset;
 
     private void Start()
     {
         m_nextSpawnTime = Time.time + m_minSpawnTime + Random.Range(0, m_randSpawnTimeOffset);
-        m_spawnRadiusOffset = GetComponent<SphereCollider>().radius;
     }
 
     // Update is called once per frame
-    void Update () {
-		if(Time.time > m_nextSpawnTime)
+    void Update()
+    {
+        if (Time.time > m_nextSpawnTime)
         {
             m_nextSpawnTime = Time.time + m_minSpawnTime + Random.Range(0, m_randSpawnTimeOffset);
             Vector2 randOffset = (Random.insideUnitCircle * m_spawnRadiusOffset);
             Vector3 startPosition = new Vector3(transform.position.x + randOffset.x, transform.position.y, transform.position.z + randOffset.y);
-            GameObject passerby = Instantiate(m_citizen, startPosition, transform.rotation);
+            GameObject passerby = Instantiate(m_passerby, startPosition, transform.rotation);
             Vector3 endPosition = new Vector3(m_despawnLocation.transform.position.x + randOffset.x, m_despawnLocation.transform.position.y, m_despawnLocation.transform.position.z + randOffset.y);
             passerby.GetComponent<Passerby>().SetTargetLocation(endPosition);
         }
-	}
+    }
 }
