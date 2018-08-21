7a6ec8af3ac6b15ac03da2124c8708207d3b82ab
MDS\sean.hua7281 <>
2018-08-21 12:50:37 +1200

    Got cat colouring, shouldnt base materials

--- MamaBunny/Assets/Scripts/Rabboids/Rabboid.cs
+++ MamaBunny/Assets/Scripts/Rabboids/Rabboid.cs
@@ -14,22 +14,27 @@ public class Rabboid : MonoBehaviour {
     public Transform m_defaultBack;
     public Transform m_defaultMouth;
 
+    public Transform[] m_otherBodyParts;
+
     public void Initialize(RabboidResult _result)
     {
+        Transform newBack = null, newMouth = null;
+
         // Set back
         if (_result.m_backPart)
-            Instantiate(_result.m_backPart.m_bodyPrefab, m_backPoint.position, m_backPoint.rotation, m_backPoint.parent);
+            newBack = Instantiate(_result.m_backPart.m_bodyPrefab, m_backPoint.position, m_backPoint.rotation, m_backPoint.parent);
         else if (m_defaultBack)
-            Instantiate(m_defaultBack, m_backPoint.position, m_backPoint.rotation, m_backPoint.parent);
+            newBack = Instantiate(m_defaultBack, m_backPoint.position, m_backPoint.rotation, m_backPoint.parent);
 
         // Set mouth
         if (_result.m_mouthPart)
-            Instantiate(_result.m_mouthPart.m_bodyPrefab, m_mouthPoint.position, m_mouthPoint.rotation, m_mouthPoint.parent);
+            newMouth = Instantiate(_result.m_mouthPart.m_bodyPrefab, m_mouthPoint.position, m_mouthPoint.rotation, m_mouthPoint.parent);
         else if (m_defaultMouth)
-            Instantiate(m_defaultMouth, m_mouthPoint.position, m_mouthPoint.rotation, m_mouthPoint.parent);
+            newMouth = Instantiate(m_defaultMouth, m_mouthPoint.position, m_mouthPoint.rotation, m_mouthPoint.parent);
 
         // Set colour
-
+        if (_result.m_resultColour)
+            SetColours(_result.m_resultColour, newBack, newMouth);
 
         // Set size
         transform.localScale = transform.localScale * _result.m_size;
@@ -37,6 +42,44 @@ public class Rabboid : MonoBehaviour {
 
         // Output name
         Debug.Log("New Rabboid: " + _result.m_name);
+    }
+
+    void SetColours(RabboidColour _colours, Transform _newBack, Transform _newMouth)
+    {
+        // Set colours - body
+        Material newBodyMat = new Material(GetComponent<Renderer>().sharedMaterial);
+        newBodyMat.color = _colours.m_color;
+        GetComponent<Renderer>().sharedMaterial = newBodyMat;
+
+        // Set colours - extra features
+        foreach (Transform t in m_otherBodyParts)
+        {
+            newBodyMat = new Material(t.GetComponent<Renderer>().sharedMaterial);
+            newBodyMat.color = _colours.m_color;
+            t.GetComponent<Renderer>().sharedMaterial = newBodyMat;
+        }
+
+        if (_newBack)
+        {
+            Renderer[] rendList = _newBack.GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in rendList)
+            {
+                newBodyMat = new Material(r.sharedMaterial);
+                newBodyMat.color = _colours.m_color;
+                r.sharedMaterial = newBodyMat;
+            }
+        }
+
+        if (_newMouth)
+        {
+            Renderer[] rendList = _newMouth.GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in rendList)
+            {
+                newBodyMat = new Material(r.sharedMaterial);
+                newBodyMat.color = _colours.m_color;
+                r.sharedMaterial = newBodyMat;
+            }
+        }
     }
 
 }
