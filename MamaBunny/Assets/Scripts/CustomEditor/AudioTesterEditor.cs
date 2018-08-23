2f1035706066cc97e834db8566d503f206bfad49
MDS\sean.hua7281 <>
2018-08-24 11:57:47 +1200

    created an audio tester thing

---
+++ MamaBunny/Assets/Scripts/CustomEditor/AudioTesterEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AudioTester), true)]
+public class AudioTesterEditor : Editor
+{
+    [SerializeField]
+    private AudioSource m_previewer;
+
+    public void OnEnable()
+    {
+        m_previewer = EditorUtility.CreateGameObjectWithHideFlags("Audio preview", HideFlags.HideAndDontSave, typeof(AudioSource)).GetComponent<AudioSource>();
+    }
+
+    public void OnDisable()
+    {
+        DestroyImmediate(m_previewer.gameObject);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+        if (GUILayout.Button("Play Audio"))
+        {
+            ((AudioTester)target).Play(m_previewer);
+        }
+        if (GUILayout.Button("Reset values"))
+        {
+            ((AudioTester)target).Reset();
+        }
+        EditorGUI.EndDisabledGroup();
+    }
+}
---
+++ MamaBunny/Assets/Scripts/ScriptableObjects/AudioTester.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AudioTester/TesterThing")]
+public class AudioTester : ScriptableObject {
+
+    public RangedFloat m_volume;
+    [MinMaxRange(0,2)]
+    public RangedFloat m_pitch;
+    public AudioClip m_audioClip;
+
+    public void Play(AudioSource _source)
+    {
+        _source.clip = m_audioClip;
+        _source.volume = Random.Range(m_volume.minValue, m_volume.maxValue);
+        _source.pitch = Random.Range(m_pitch.minValue, m_pitch.maxValue);
+        _source.loop = false;
+        _source.Play();
+    }
+
+    public void Reset()
+    {
+        m_volume.minValue = m_volume.maxValue = m_pitch.minValue = m_pitch.maxValue = 1;
+    }
+}
