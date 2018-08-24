622d789d2542090b1b98f42609f89fe38042b30c
MDS\sean.hua7281 <>
2018-08-24 17:39:42 +1200

    created food getting sucked in animation

---
+++ MamaBunny/Assets/Scripts/Items/Pick Up Items/ItemMovePath.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMovePath : MonoBehaviour {
+
+    public Transform[] m_movePath;
+
+    // SHOULD ONLY COLLIDE WITH ITEMS.
+    // ONLY PICKUPS SHOULD BE ON THE PICKUP LAYER
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIGGERED OwO");
+        other.GetComponent<PickUp>().OnBeingEaten(m_movePath);
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw all nodes
+        Gizmos.color = Color.red;
+        foreach (Transform t in m_movePath)
+            Gizmos.DrawWireSphere(t.position, 0.03f);
+
+        if (m_movePath.Length < 2)
+            return;
+
+        // Draw all paths
+        Gizmos.color = Color.green;
+        for (int i = 1; i < m_movePath.Length; i++)
+            Gizmos.DrawLine(m_movePath[i - 1].position, m_movePath[i].position);
+
+    }
+}
--- MamaBunny/Assets/Scripts/Items/PickUp.cs
+++ MamaBunny/Assets/Scripts/Items/PickUp.cs
@@ -6,6 +6,7 @@ public abstract class PickUp : MonoBehaviour {
 
     public float m_liveTime = 20;
     float m_startLiveTime;
+    bool m_beingEaten = false;
 
     private void Awake()
     {
@@ -14,9 +15,58 @@ public abstract class PickUp : MonoBehaviour {
 
     private void Update()
     {
+        if (m_beingEaten)
+            return;
+
         if (Time.time - m_startLiveTime >= m_liveTime)
             Destroy(gameObject);
     }
 
     public abstract void OnEatenByMamaRabbit(RabboidMama _mama);
+
+    public void OnBeingEaten(Transform[] _pathToFollow)
+    {
+        if (m_beingEaten)
+            return;
+
+        m_beingEaten = true;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb) rb.isKinematic = true;
+
+        StartCoroutine(FollowPipePath(_pathToFollow));
+    }
+
+    IEnumerator FollowPipePath(Transform[] _pathToFollow)
+    {
+        float moveSpeed = 3.5f;
+        Vector3 rotateAxis = new Vector3(1, 0.6f, 0.3f);
+        float rotateSpeed = 45f;
+
+        for (int i = 0; i < _pathToFollow.Length; i++)
+        {
+            bool done = false;
+            while (!done)
+            {
+                // Move logic
+                Vector3 newPos = Vector3.MoveTowards(transform.position, _pathToFollow[i].position, moveSpeed * Time.deltaTime);
+                if (newPos == _pathToFollow[i].position)
+                {
+                    if (i + 1 < _pathToFollow.Length - 1)
+                        newPos = Vector3.MoveTowards(transform.position, _pathToFollow[i + 1].position, moveSpeed * Time.deltaTime);
+                    done = true;
+                }
+                transform.position = newPos;
+
+                // Rotate logic
+                transform.Rotate(rotateAxis * rotateSpeed * Time.deltaTime);
+                yield return null;
+            }
+        }
+
+        // Start moving again
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb) rb.isKinematic = false;
+
+        yield return null;
+    }
 }
