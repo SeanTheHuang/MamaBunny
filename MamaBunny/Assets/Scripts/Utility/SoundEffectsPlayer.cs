b167e7faa88ad63ca5502e269e2dc00e70124f89
Sean Huang <sean.the.huang@gmail.com>
2018-08-25 17:16:55 +1200

    extended my audio player

--- MamaBunny/Assets/Scripts/Utility/SoundEffectsPlayer.cs
+++ MamaBunny/Assets/Scripts/Utility/SoundEffectsPlayer.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct SoundClip
+public struct AudioGroup
 {
-    public AudioClip m_audioClip;
     public string m_name;
+    public RangedFloat m_volume;
+    [MinMaxRange(0, 2)]
+    public RangedFloat m_pitch; 
+    public AudioClip[] m_audioClips; // Chooses one out of these
 }
 
 public class SoundEffectsPlayer : MonoBehaviour {
 
-    public SoundClip[] m_initalClips;
-    Dictionary<string, AudioClip> m_soundDict;
+    public AudioGroup[] m_initalClips;
+    Dictionary<string, AudioGroup> m_audioDict;
 
     public static SoundEffectsPlayer Instance
     { get; private set; }
@@ -26,16 +29,26 @@ public class SoundEffectsPlayer : MonoBehaviour {
         m_source = GetComponent<AudioSource>();
 
         // Initialize soundDict
-        m_soundDict = new Dictionary<string, AudioClip>();
-        foreach (SoundClip sc in m_initalClips)
+        m_audioDict = new Dictionary<string, AudioGroup>();
+        foreach (AudioGroup sc in m_initalClips)
         {
-            m_soundDict.Add(sc.m_name, sc.m_audioClip);
+            m_audioDict.Add(sc.m_name, sc);
         }
     }
 
-    public void PlaySound(string _name)
+    public void PlaySound(string _name, AudioSource _source = null)
     {
-        m_source.clip = m_soundDict[_name];
-        m_source.Play();
+        AudioSource usedSource = _source ? _source : m_source;
+
+        // Get correct audio group
+        AudioGroup group = m_audioDict[_name];
+
+        // Randomize clip, volume, and pitch
+        int clipIndex = Random.Range(0, group.m_audioClips.Length);
+        usedSource.volume = Random.Range(group.m_volume.minValue, group.m_volume.maxValue);
+        usedSource.pitch = Random.Range(group.m_volume.minValue, group.m_volume.maxValue);
+
+        // Play
+        m_source.PlayOneShot(group.m_audioClips[clipIndex]);
     }
 }
