ffc786d322127259aac862786d21ba3885656d37
MDS\hugo.ada7271 <hugogfadams@gmail.com>
2018-08-27 11:17:39 +1200

    invetory should save between scenes

---
+++ MamaBunny/Assets/Scripts/ScriptableObjects/SaveInventory_Player.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "InventorySO/inv_player")]
+public class SaveInventory_Player : ScriptableObject
+{
+    public List<RabboidModBase> m_inventory;
+    public uint m_capacity;
+
+}
