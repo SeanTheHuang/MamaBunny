e326f2a691715e7b6a6bea8d04c87b4163e6edb4
MDS\sean.hua7281 <>
2018-08-28 11:29:04 +1200

    Retical. updated bullets

--- MamaBunny/Assets/Scripts/Bullet.cs
+++ MamaBunny/Assets/Scripts/Bullet.cs
@@ -6,15 +6,17 @@ public class Bullet : MonoBehaviour {
 
     public float m_intialSpeed = 60;
     public float m_damage = 10;
+    public float m_maxLife = 10;
 
     public void Initialize(Vector3 _dir)
     {
         GetComponent<Rigidbody>().velocity = _dir * m_intialSpeed;
+        Destroy(gameObject, m_maxLife); // Destroy bullet if alive too long
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider c)
     {
-        GunTarget gt = collision.transform.GetComponent<GunTarget>();
+        GunTarget gt = c.transform.GetComponent<GunTarget>();
         if (gt)
             gt.TakeHit(m_damage);
 
