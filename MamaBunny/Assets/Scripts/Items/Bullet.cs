0447fe9c6b87afc728302b3cdd45a0280912f20d
MDS\sean.hua7281 <>
2018-08-28 10:23:50 +1200

    made gun shoot bullets

---
+++ MamaBunny/Assets/Scripts/Items/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float m_damage = 10;
+    public float m_initialSpeed = 715;
+
+    public void Initialize(Vector3 _direction)
+    {
+        GetComponent<Rigidbody>().velocity = _direction * m_initialSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GunTarget target = collision.transform.GetComponent<GunTarget>();
+
+        if (target)
+            target.TakeHit(m_damage);
+
+        Destroy(gameObject);
+    }
+}
