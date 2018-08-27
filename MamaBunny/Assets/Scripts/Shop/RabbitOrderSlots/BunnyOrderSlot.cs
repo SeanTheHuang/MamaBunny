deba17f14da569b8a146ef880037113c3c800eea
prunefriend <jwjmair@gmail.com>
2018-08-27 18:07:33 +1200

    Order slots

--- MamaBunny/Assets/Scripts/Shop/RabbitOrderSlots/BunnyOrderSlot.cs
+++ MamaBunny/Assets/Scripts/Shop/RabbitOrderSlots/BunnyOrderSlot.cs
@@ -14,7 +14,7 @@ public class BunnyOrderSlot : MonoBehaviour {
     public struct RabboidOrder
     {
         public float m_size;
-        public RabboidColour m_colour;
+        public Color m_colour;
         public RabboidBodyPart m_mouthPart;
         public RabboidBodyPart m_backPart;
     }
@@ -24,7 +24,9 @@ public class BunnyOrderSlot : MonoBehaviour {
     // Use this for initialization
     void Start () {
         m_bunnyOrderController = transform.GetComponentInParent<BunnyOrderController>();
-	}
+        m_rabboidOrder.m_colour = Color.white;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -72,18 +74,18 @@ public class BunnyOrderSlot : MonoBehaviour {
                 {
                     if (Random.Range(0, 2) == 0)
                     {
-                        m_rabboidOrder.m_size = 1.3f;
+                        m_rabboidOrder.m_size = RabboidCalculator.LARGE_SIZE;
                     }
                     else
                     {
-                        m_rabboidOrder.m_size = 0.7f;
+                        m_rabboidOrder.m_size = RabboidCalculator.SMALL_SIZE;
                     }
                 }
                 // Chose a colour
                 else if (ingredientType == 1)
                 {
                     int randomColor = Random.Range(0, possibleColours.Length);
-                    m_rabboidOrder.m_colour = possibleColours[randomColor];
+                    m_rabboidOrder.m_colour = possibleColours[randomColor].m_color;
                 }
                 // Chose a mouthpart
                 else if (ingredientType == 2)
@@ -109,14 +111,96 @@ public class BunnyOrderSlot : MonoBehaviour {
     {
         if (m_isActive)
         {
+            RabboidResult RabboidStats = other.transform.parent.GetComponent<Rabboid>().RabboidStats;
             // Check the order
+            int rabbitScore = 20; // Score is given based on how close the rabbit is to what the order wanted.
+            int negativePointModifier = 5;
+            int positivePointModifier = 10;
+
+            // Check Size
+            if (m_rabboidOrder.m_size == 0)
+            {
+                if (RabboidStats.m_size <= RabboidCalculator.SMALL_SIZE || RabboidStats.m_size >= RabboidCalculator.LARGE_SIZE)
+                {
+                    rabbitScore -= negativePointModifier;
+                }
+                else
+                {
+                    rabbitScore += positivePointModifier;
+                }
+            }
+            if(m_rabboidOrder.m_size == RabboidCalculator.SMALL_SIZE)
+            {
+                if(RabboidStats.m_size <= RabboidCalculator.SMALL_SIZE)
+                {
+                    rabbitScore += positivePointModifier;
+                }
+                else if (RabboidStats.m_size >= RabboidCalculator.LARGE_SIZE)
+                {
+                    rabbitScore -= negativePointModifier * 2;
+                }
+                else
+                {
+                    rabbitScore -= negativePointModifier;
+                }
+            }
+            if (m_rabboidOrder.m_size == RabboidCalculator.LARGE_SIZE)
+            {
+                if (RabboidStats.m_size >= RabboidCalculator.LARGE_SIZE)
+                {
+                    rabbitScore += positivePointModifier;
+                }
+                else if (RabboidStats.m_size <= RabboidCalculator.SMALL_SIZE)
+                {
+                    rabbitScore -= negativePointModifier * 2;
+                }
+                else
+                {
+                    rabbitScore -= negativePointModifier;
+                }
+            }
+
+            // Check Color
+            Color resultsColor = Color.white;
+            if (RabboidStats.m_resultColour != null)
+                resultsColor = RabboidStats.m_resultColour.m_color;
+            if(resultsColor == m_rabboidOrder.m_colour)
+            {
+                rabbitScore += positivePointModifier;
+            }
+            else
+            {
+                rabbitScore -= negativePointModifier;
+            }
+
+            // Check Body
+            if((RabboidStats.m_backPart == null && m_rabboidOrder.m_backPart == null)
+            || (RabboidStats.m_backPart != null && m_rabboidOrder.m_backPart != null))
+            {
+                rabbitScore += positivePointModifier;
+            }
+            else
+            {
+                rabbitScore -= negativePointModifier;
+            }
+
+            // Check Head
+            if((RabboidStats.m_mouthPart == null && m_rabboidOrder.m_mouthPart == null)
+            || (RabboidStats.m_mouthPart != null && m_rabboidOrder.m_mouthPart != null))
+            {
+                rabbitScore += positivePointModifier;
+            }
+            else
+            {
+                rabbitScore -= negativePointModifier;
+            }
 
             // Add coins based on performance
+            Debug.Log("New Coin Total: " + rabbitScore);
 
             // Tidy up
             Destroy(other.gameObject);
             m_isActive = false;
-            Debug.Log("RabbitSold!");
         }
     }
 }
