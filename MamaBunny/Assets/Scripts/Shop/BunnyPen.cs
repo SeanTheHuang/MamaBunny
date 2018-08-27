393e9e21309f5de21fa951d15c672f9bc0ab0298
MDS\hugo.ada7271 <hugogfadams@gmail.com>
2018-08-28 10:44:08 +1200

    bunny pen confusing functionality

---
+++ MamaBunny/Assets/Scripts/Shop/BunnyPen.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyPen : MonoBehaviour {
+
+    //rotate between 0, and 112 on Z axis
+    Animation m_openAni;
+    public uint m_rabbitsInside = 0;
+
+    bool m_rotating = false;
+	void Start () {
+        m_openAni = GetComponent<Animation>();
+        if(m_rabbitsInside != 0)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Open()
+    {
+        m_openAni.PlayQueued("bunnyPen");
+    }
+
+    public void Close()
+    {
+        m_openAni.PlayQueued("bunnyPenClose");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.GetComponentInParent<Rabboid>())
+        {
+            Debug.Log("HONK HONK");
+            //m_rabbitsInside++;
+            Close();
+        }
+    }
+
+}
