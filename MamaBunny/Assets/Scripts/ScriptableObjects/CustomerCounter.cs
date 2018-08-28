8472f2a89d31eff5082f3bea675d3e1f77aee764
prunefriend <jwjmair@gmail.com>
2018-08-28 17:25:51 +1200

    Made the customer counter a scriptable object so it does not reset between scenes

---
+++ MamaBunny/Assets/Scripts/ScriptableObjects/CustomerCounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Customer/Counter")]
+public class CustomerCounter : ScriptableObject
+{
+    public int m_cusomterCounter;
+
+    private void OnEnable()
+    {
+        m_cusomterCounter = 0;
+    }
+}
