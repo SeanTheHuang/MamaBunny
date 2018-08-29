8336e7c281889538e76b0186dde08e14e31d2843
prunefriend <jwjmair@gmail.com>
2018-08-29 17:51:58 +1200

    Rabbits are saved inside their cages

---
+++ MamaBunny/Assets/Scripts/ScriptableObjects/BunnyPenData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "BunnyPen/PenData")]
+public class BunnyPenData : ScriptableObject
+{
+
+    public bool m_bunnyInside;
+    public float m_bunnySize;
+    public RabboidColour m_bunnyColour;
+    public RabboidBodyPart m_bunnyMouthPart;
+    public RabboidBodyPart m_bunnyBackPart;
+
+    private void OnEnable()
+    {
+
+    }
+
+    public void ResetVariables()
+    {
+        m_bunnyInside = false;
+        m_bunnySize = 0;
+        m_bunnyColour = null;
+        m_bunnyMouthPart = null;
+        m_bunnyBackPart = null;
+    }
+}
--- MamaBunny/Assets/Scripts/Shop/BunnyPen.cs
+++ MamaBunny/Assets/Scripts/Shop/BunnyPen.cs
@@ -6,33 +6,29 @@ public class BunnyPen : MonoBehaviour {
 
     //rotate between 0, and 112 on Z axis
     Animation m_openAni;
-    public uint m_rabbitsInside = 0;
 
-    bool m_rotating = false;
+    public BunnyPenData m_penData;
+    public GameObject m_baseRabboidPrefab;
+
 	void Start () {
+        Debug.Log(gameObject.name);
         m_openAni = GetComponent<Animation>();
-        if(m_rabbitsInside != 0)
+        if(m_penData.m_bunnyInside)
         {
             Close();
+            GameObject rabboid = Instantiate(m_baseRabboidPrefab, transform.position, transform.rotation);
+            RabboidResult RabboidStats = new RabboidResult();
+            RabboidStats.m_resultColour = m_penData.m_bunnyColour;
+            RabboidStats.m_size = m_penData.m_bunnySize;
+            RabboidStats.m_mouthPart = m_penData.m_bunnyMouthPart;
+            RabboidStats.m_backPart = m_penData.m_bunnyBackPart;
+            rabboid.GetComponent<Rabboid>().Initialize(RabboidStats);
         }
         else
         {
             Open();
         }
 	}
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.N))
-        {
-            Open();
-        }
-        if(Input.GetKeyDown(KeyCode.M))
-        {
-            Close();
-        }
-    }
 
     public void Open()
     {
@@ -46,10 +42,15 @@ public class BunnyPen : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.transform.GetComponentInParent<Rabboid>())
+        if(!m_penData.m_bunnyInside && other.transform.GetComponentInParent<Rabboid>())
         {
-            Debug.Log("HONK HONK");
-            //m_rabbitsInside++;
+            Debug.Log("Called");
+            RabboidResult RabboidStats = other.transform.parent.GetComponent<Rabboid>().RabboidStats;
+            m_penData.m_bunnyInside = true;
+            m_penData.m_bunnyColour = RabboidStats.m_resultColour;
+            m_penData.m_bunnySize = RabboidStats.m_size;
+            m_penData.m_bunnyMouthPart = RabboidStats.m_mouthPart;
+            m_penData.m_bunnyBackPart = RabboidStats.m_backPart;
             Close();
         }
     }
