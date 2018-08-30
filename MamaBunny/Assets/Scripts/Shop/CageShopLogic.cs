d800e133e840de5fbbf1f65774511e0c1be3baa7
MDS\sean.hua7281 <>
2018-08-30 17:38:06 +1200

    cage shop logic now done

--- MamaBunny/Assets/Scripts/Shop/CageShopLogic.cs
+++ MamaBunny/Assets/Scripts/Shop/CageShopLogic.cs
@@ -5,9 +5,9 @@ using TMPro;
 
 public class CageShopLogic : MonoBehaviour {
 
-    [MinMaxRange(3.0f, 15.0f)]
+    [MinMaxRange(0.0f, 30.0f)]
     public RangedFloat m_timeToSell;
-    [MinMaxRange(4.0f, 10.0f)]
+    [MinMaxRange(0.0f, 30.0f)]
     public RangedFloat m_sellAmount;
     public BunnyPenData[] m_pens;
     public SaveInventory_Player m_inventory;
@@ -58,7 +58,7 @@ public class CageShopLogic : MonoBehaviour {
         }
 
         int index = Random.Range(0, m_filledList.Count);
-
+        m_nextSellTime = Time.time + Random.Range(m_timeToSell.minValue, m_timeToSell.maxValue);
         BunnyPenData pen = m_filledList[index];
         m_filledList.Remove(pen);
         SellPen(pen);
@@ -67,6 +67,7 @@ public class CageShopLogic : MonoBehaviour {
     void SellPen(BunnyPenData pen)
     {
         pen.ResetVariables();
+        SoundEffectsPlayer.Instance.PlaySound("kaching");
         uint sellAmount = (uint)Mathf.RoundToInt(Random.Range(m_sellAmount.minValue, m_sellAmount.maxValue));
         m_inventory.m_money += sellAmount;
         m_moneyText.text = "Coins: " + m_inventory.m_money;
